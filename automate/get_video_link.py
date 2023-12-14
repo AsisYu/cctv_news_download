1a853836e0b5855092121da2402ec8e01b0d3ae9
Murphy-Yan <117812045+Murphy-Yan@users.noreply.github.com>
2023-12-14 14:46:58 +0800

    Update get_video_link.py
    破解之前的固定值问题，已找到数值来源，已替换，增加网页重试3次，提高稳定性

--- automate/get_video_link.py
+++ automate/get_video_link.py
@@ -1,14 +1,14 @@
 from hashlib import md5
 import os
 import re
+import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
-import random
-import string
+from selenium.common.exceptions import TimeoutException
 import calendar
 
 
@@ -25,7 +25,7 @@ def get_all_days(year, month):
 
     return dates
 
-def get_uid(url):
+def get_uid_and_vdnAdStaticCheck(url):
     """
     获取指定网页的cookie信息
     """
@@ -33,10 +33,21 @@ def get_uid(url):
     driver = webdriver.Edge()
     # 打开网页
     driver.get(url)
-    # 等待网页加载完成
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+    # 设置最大重试次数
+    max_retries = 3
+    retry_count = 0
+    while retry_count < max_retries:
+        try:
+            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+            # 如果成功找到元素，则继续执行后续代码
+            break
+        except TimeoutException:
+            # 如果超时，刷新页面
+            driver.refresh()
+            retry_count += 1
     # 获取当前页面的cookie信息
     document_cookie = driver.execute_script("return document.cookie")
+    vdnAdStaticCheck = driver.execute_script("return vodh5player.VDN_AD_STATIC_CHECK")
     # 关闭浏览器
     driver.quit()
     # 使用正则表达式提取 Fingerprint 的值
@@ -44,11 +55,11 @@ def get_uid(url):
     match = re.search(pattern, document_cookie)
     if match:
         fingerprint_value = match.group(1)
-        return fingerprint_value
+        return {"uid":fingerprint_value,"vdnAdStaticCheck":vdnAdStaticCheck}
     else:
-        raise("未能从第一个视频网页的document.cookie里找到uid(fingerprint_value)")
+        raise ("未能从第一个视频网页的document.cookie里找到uid(fingerprint_value)")
 
-def get_cctv_pid_link(url):
+def get_cctv_pids_and_links(url):
     """
     输出一个字典,分别为pid_list和link_list
     """
@@ -58,7 +69,16 @@ def get_cctv_pid_link(url):
     browser.get(url)
     # 等待 class 为 "jvedio" 的 div 元素出现
     wait = WebDriverWait(browser, 20)
-    element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'jvedio')))
+    max_attempts = 3
+    attempts = 0
+    while attempts < max_attempts:
+        try:
+            element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'jvedio')))
+            break  # 如果成功找到元素，则跳出循环
+        except TimeoutException:
+            attempts += 1
+            browser.refresh()  # 刷新网页
+            time.sleep(10)  # 等待10秒再进行下一次尝试
     html = browser.page_source
     # 解析页面内容，获取jvedio元素
     soup = BeautifulSoup(html, 'html.parser')
@@ -71,12 +91,11 @@ def get_cctv_pid_link(url):
     for jvedio in jvedios:
         src = jvedio.find('img')['src']
         pid = os.path.splitext(src.split('/')[-1].split('-')[0])[0]
-        if len(pid) > 31:
+        if len(pid) == 32:
             pid_list.append(pid)
             link = jvedio.find('a')['lanmu1']
             link_list.append(link)
-    return {"pid_list":pid_list,"link_list":link_list}
-
+    return {"pid_list": pid_list, "link_list": link_list}
 if __name__ == "__main__":
     # 用户输入年份和月份
     year = int(input("请输入你想下载的年份："))
@@ -102,22 +121,18 @@ if __name__ == "__main__":
         print(f"开始{year_str}-{month_str}-{day_str}的获取")
 
         url = f'https://search.cctv.com/search.php?qtext=%E6%96%B0%E9%97%BB%E8%81%94%E6%92%AD{year_str}{month_str}{day_str}&type=video'
-        print("url="+url)
-        pid_and_link = get_cctv_pid_link(url)
-        print(pid_and_link)
-        uid = get_uid(pid_and_link["link_list"][0])
-        print("uid:"+uid)
+        # 获得pids_and_links
+        pids_and_links = get_cctv_pids_and_links(url)
+        print(pids_and_links)
+        # 获得uid_and_vdnAdStaticCheck
+        uid_and_vdnAdStaticCheck = get_uid_and_vdnAdStaticCheck(pids_and_links["link_list"][0])
+        print(uid_and_vdnAdStaticCheck)
 
-        chars = string.ascii_uppercase + string.digits
-        uid=''.join(random.choice(chars) for _ in range(32))
-        for pid in pid_and_link["pid_list"]:
+        for pid in pids_and_links["pid_list"]:
             timestamp = str(int(datetime.now().timestamp()))[:10]
-            # 此处的47899B86370B879139C08E7A3B5E8826在网页源代码中为固定值,不知后续会不会变化
-            md5_p = md5((timestamp + "2049" + "47899B86370B879139C08E7A3B5E8826" + uid).encode('utf-8')) #47899B86370B879139C08EA3B5E88267
-            md5_p=md5_p.hexdigest().upper()
-            video_link = "https://"+"vdn.apps.cntv.cn/api/getHttpVideoInfo.do?"+"pid=" +pid +"&client=flash&im=0&tsp="+timestamp+"&vn=2049&vc=" +md5_p+ "&uid=" + uid + "&wlan="
+            md5_p = md5((timestamp + "2049" + uid_and_vdnAdStaticCheck["vdnAdStaticCheck"] +uid_and_vdnAdStaticCheck["uid"]).encode('utf-8'))
+            md5_p = md5_p.hexdigest().upper()
+            video_link = "https://"+"vdn.apps.cntv.cn/api/getHttpVideoInfo.do?"+"pid=" + pid + \
+                "&client=flash&im=0&tsp="+timestamp + \
+                "&vn=2049&vc=" + md5_p + "&uid=" + uid_and_vdnAdStaticCheck["uid"] + "&wlan="
             print(video_link)
-            # 写入 video_link 到 get_link.txt 文件
-            with open(link_file, 'a') as f:
-                f.write(year_str+"-"+month_str+"-"+day_str+":"+video_link + '\n')
-                break   # 只写入第一行内容
