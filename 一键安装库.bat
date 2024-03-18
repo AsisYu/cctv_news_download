716c9a8bbeb3db9677a91022498023dc7a6b6e46
Qin <68932312+AsisYu@users.noreply.github.com>
2024-03-18 13:09:41 +0800

    检查拓展并自动安装
    检查拓展并自动安装

---
+++ 一键安装库.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM 设置控制台编码为UTF-8
+chcp 65001
+
+REM 定义需要安装的Python包
+set "PACKAGES=selenium beautifulsoup4 regex requests glob2 moviepy"
+
+REM 循环遍历所有包并尝试安装，使用阿里云的镜像源
+for %%p in (%PACKAGES%) do (
+    echo 正在检查并尝试安装 %%p...
+    pip install %%p -i https://mirrors.aliyun.com/pypi/simple/
+)
+
+echo.
+echo 所有必要的Python包安装检查完成。
+pause
